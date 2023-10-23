499ddf561db334437955cecb189eef0198f98a43
Ghadrr <105183365+Ghadrr@users.noreply.github.com>
2023-10-23 18:47:41 -0300

    cmt

--- estoque_proj_aula/urls.py
+++ estoque_proj_aula/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('inicio/', views.index, name='home')
+    path('inicio/', views.index, name='home'),
+    path('product-detail/<int:id>/', views.product_detail , name='product-detail')
 ]
--- estoque_proj_aula/views.py
+++ estoque_proj_aula/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse
+from .models import Products
 # Create your views here.
 def index(request):
-    return HttpResponse('Estou no Django')
+    produtos = Products.objects.all()
+
+    # for produto in produtos:
+    #     print(produto.name)
+    
+    return render(request, 'pages/index.html.', {'produtos':produtos})
+
+def product_detail(request, id):
+    product = Products.objects.get(id=id)
+    
+    return render(request, 'pages/product_detail.html',{'product':product})
